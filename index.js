0e208b46bd6414f3bd9b7e1a43e6310abcd46d37
Manuel Montoya <manuelm1209@hotmail.com>
2022-11-20 18:49:37 -0500

    Functional menus

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 import { scatterPlot } from './scatterPlot.js';
+import { menu } from './menu.js';
 
 const csvUrl = "https://gist.githubusercontent.com/curran/a08a1080b88344b0c8a7/raw/0e7a9b0a5d22642a06d3d5b9bcbad9890c8ee534/iris.csv";
 
@@ -14,11 +15,19 @@ const parseRow = (d) => {
 const width = window.innerWidth;
 const height = window.innerHeight;    
 
-const svg = d3.select('body').append('svg')
+const svg = d3.select('body')
+    .append('svg')
     .attr('width', width)
     .attr('height', height);
+
+const menuContainer = d3.select('body')
+    .append('div')
+    .attr('class', 'menu-container');
     
-// // The second argument is a funtion that takes as input a single row and replace it.  
+const xMenu = menuContainer.append('div');
+const yMenu = menuContainer.append('div');
+    
+    // // The second argument is a funtion that takes as input a single row and replace it.  
 // // First way to import data.
 // d3.csv(csvUrl, parseRow).then(data => {
 //     console.log(data);
@@ -32,28 +41,27 @@ const main = async () => {
     .height(height)
     .data(data)
     // Columns for the range.
-    .xValue((d) => d.petal_length)
-    .yValue((d) => d.sepal_length)
+    .xValue((d) => d.petal_width)
+    .yValue((d) => d.petal_width)
     // d3 margin convention.
     .margin({top: 20, right: 20, bottom: 40, left:50})
     svg.call(plot);
 
-    const columns = [
-        'petal_width',
-        'sepal_width',
-        'petal_length',
-        'sepal_length'
+    const options = [
+        { value: 'petal_width', text: 'Petal Width' },
+        { value: 'sepal_width', text: 'Sepal Width' },
+        { value: 'petal_length', text: 'Petal Lenght' },
+        { value: 'sepal_length', text: 'Sepal Length' }
     ];
 
-    let i = 0;
 
-    setInterval(() => {
-        console.log(i % columns.length);
-        plot.xValue((d) => d[columns[i % columns.length]])
-        svg.call(plot);
-        i++;
-    }, 4000);
-
+    // Menus
+    xMenu.call(menu().id('x-menu').labelText('X:').options(options).on('change', column => {
+        svg.call(plot.xValue(d => d[column]));
+    } ));
+    yMenu.call(menu().id('y-menu').labelText('Y:').options(options).on('change', column => {
+        svg.call(plot.yValue(d => d[column]));
+    } ));
 
 };
 main();
